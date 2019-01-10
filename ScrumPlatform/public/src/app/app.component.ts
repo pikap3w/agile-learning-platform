64e1af31a6dc1d9dfbe977e97be68a816a976f4a
GrammySpaghetti <ryanchuray@me.com>
2019-01-10 10:30:51 -0600

    pre routing set up

--- ScrumPlatform/public/src/app/app.component.ts
+++ ScrumPlatform/public/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpService } from './http.service';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'public';
+  constructor(private _httpService: HttpService){}
 }

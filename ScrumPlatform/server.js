deb7ecef065681ddfe26440b83a80bb9c8fb683f
Kristen <29871623+pikap3w@users.noreply.github.com>
2019-01-11 11:20:48 -0600

    changes to dependencies, server.js, app.component

--- ScrumPlatform/server.js
+++ ScrumPlatform/server.js
@@ -1,19 +1,20 @@
-var express    = require('express'),
-    app        = express(),
-    path       = require('path'),
-    bodyParser = require('body-parser'),
-    mongoose   = require('./config/mongoose'),
-    port       = 8000;
+const express = require('express'),
+		app = express(),
+		path = require('path'),
+		bodyParser = require('body-parser'),
+		port = 8000;
 
+		require('./config/mongoose');
 
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static( __dirname + '/public/dist/public' ));
+app.use(express.static(path.join(__dirname + '/public/dist/public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 // app.use(express.static(path.join(__dirname, 'static')));
-app.set(path.join('views', __dirname, 'views'));
-app.set('view engine', 'ejs');
+// app.set(path.join('views', __dirname, 'views'));
+// app.set('view engine', 'ejs');
+
 require('./config/routes.js')(app);
 
-app.listen(port, function() {
-    console.log(`listening on port ${port}`);
-})
+app.listen(port, function () {
+	console.log(`listening on port ${port}`);
+});

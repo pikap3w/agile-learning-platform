c0fb5eafe22eac4aef43b0279ca4756cd88dda81
ZPieper <pieperzachary@gmail.com>
2019-01-11 14:31:03 -0600

    login/reg additions

--- ScrumPlatform/config/routes.js
+++ ScrumPlatform/config/routes.js
@@ -4,6 +4,8 @@ var path = require('path');
 
 module.exports = function(app) {
   app.get('/users', users_controller.users);
+  app.post('/register', users_controller.register);
+  app.post('/login', users_controller.login);
   app.all("*", (req,res,next) => {
     res.sendFile(path.resolve("./public/dist/public/index.html"))
   });
